3541ced8e175598c06f473a6a83651eaf2ac91fc
Mohammad Azam <78340188+mohammad-azam22@users.noreply.github.com>
2024-07-30 17:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ class App:
              .stApp {{
                  background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
                  background-size: cover
+                 opacity: 0.2;
              }}
              </style>
              """,
